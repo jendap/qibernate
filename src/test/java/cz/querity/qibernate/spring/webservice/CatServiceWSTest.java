deb2690af765b857004195f430791cc61436836b
Jan Prach <jendap@gmail.com>
2011-07-10 23:25:42 +0200

    added simple test for CatServiceWS

---
+++ src/test/java/cz/querity/qibernate/spring/webservice/CatServiceWSTest.java
@@ -0,0 +1,23 @@
+package cz.querity.qibernate.spring.webservice;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+@ContextConfiguration({ "classpath:spring-persistance.xml", "classpath:spring-cxf-test.xml" })
+public class CatServiceWSTest extends AbstractJUnit4SpringContextTests {
+	@Inject
+	private CatServiceWS serviceClient;
+
+	@Test
+	public void testBookPrice() throws IOException {
+		final String response = serviceClient.clapCheerleaders();
+		assertEquals("Qk", response);
+	}
+}
