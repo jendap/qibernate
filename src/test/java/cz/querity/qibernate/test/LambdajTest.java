dbf1397c04996195aacda6ca9443074bd09fa84d
Jan Prach <jendap@gmail.com>
2011-11-14 03:46:55 +0100

    fix lambdaj example test

--- src/test/java/cz/querity/qibernate/test/LambdajTest.java
+++ src/test/java/cz/querity/qibernate/test/LambdajTest.java
@@ -1,5 +1,7 @@
 package cz.querity.qibernate.test;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +15,29 @@ import cz.querity.qibernate.model.Cat;
 public class LambdajTest {
 	@Test
 	public void testLambdaj() {
+		int expectedMinAge = Integer.MAX_VALUE;
 		final List<Cat> cats = new ArrayList<Cat>();
 		for (int i = 0; i < 10; i++) {
-			final Cat cat = new Cat("name" + i, null, (int) (Math.random() * 100));
+			final int age = (int) (Math.random() * 10);
+			final Cat cat = new Cat("name" + i, null, age);
 			cats.add(cat);
+			if (age < expectedMinAge) {
+				expectedMinAge = age;
+			}
 		}
 
-		final List<Cat> filteredCats = select(cats, having(on(Cat.class).getAge(),
-				equalTo(selectMin(cats, on(Cat.class).getAge()))));
-		System.out.println(filteredCats);
+		int minAge = ((Cat) selectMin(cats, on(Cat.class).getAge())).getAge();
+		final List<Cat> catsWithMinAge = select(cats, having(on(Cat.class).getAge(),
+				equalTo(minAge)));
+		final List<Cat> catsWithoutMinAge = select(cats, having(on(Cat.class).getAge(),
+				not(equalTo(minAge))));
+
+		for (final Cat cat : catsWithMinAge) {
+			assertEquals(expectedMinAge, cat.getAge());
+		}
+		for (final Cat cat : catsWithoutMinAge) {
+			assertFalse(expectedMinAge == cat.getAge());
+		}
+		assertEquals(cats.size(), catsWithMinAge.size() + catsWithoutMinAge.size());
 	}
 }
